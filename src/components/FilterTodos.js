9def20067a54e87da68c2018fd163b3ae1d284d1
Jesus Pachdez <pacheco1290@gmail.com>
2022-07-25 17:44:34 -0500

    I added the active counter and the clear completed

--- src/components/FilterTodos.js
+++ src/components/FilterTodos.js
@@ -1,22 +1,40 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function FilterTodos({ setStatus }) {
+function FilterTodos({ setStatus, todos, handleClearCompleted }) {
   const statusHandler = (e) => {
     setStatus(e.target.value);
   };
 
+  const activeCount = todos.filter((todo) => todo.completed === false).length;
+
   return (
-    <div onClick={statusHandler}>
-      <button value="all" type="button">
-        All
-      </button>
-      <button value="completed" type="button">
-        Completed
-      </button>
-      <button value="active" type="button">
-        Active
-      </button>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        margin: 15,
+      }}
+    >
+      <p style={{ paddingRight: 60 }}>{activeCount} items left</p>
+      <div onClick={statusHandler}>
+        <button value="all" type="button">
+          All
+        </button>
+        <button value="completed" type="button">
+          Completed
+        </button>
+        <button value="active" type="button">
+          Active
+        </button>
+      </div>
+      <p
+        style={{ paddingLeft: 60, cursor: "pointer" }}
+        onClick={handleClearCompleted}
+      >
+        Clear completed
+      </p>
     </div>
   );
 }
